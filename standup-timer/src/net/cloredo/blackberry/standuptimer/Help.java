adc3df1cd270f613a41bf5f1f47fa98b44f8500a
Carlos Loredo <ascaodeveloping@gmail.com>
2012-02-08 23:32:43 -0800

    first coomit

---
+++ standup-timer/src/net/cloredo/blackberry/standuptimer/Help.java
@@ -0,0 +1,25 @@
+package net.cloredo.blackberry.standuptimer;
+
+import net.cloredo.blackberry.standuptimer.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Help extends Activity implements OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+        View okButton = findViewById(R.id.help_ok);
+        okButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.help_ok:
+            finish();
+            break;
+        }
+    }
+}
